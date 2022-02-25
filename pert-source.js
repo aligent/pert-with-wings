3cccb83981b6b973a4558e99a1083a39382a3828
Thilina Gunasekara <thilina@aligent.com.au>
2022-02-25 13:17:11 +1030

    add support for decimal hour minute strings like 2.5h

--- pert-source.js
+++ pert-source.js
@@ -28,20 +28,23 @@ const getTotals = (arr) => {
  * input can be either hour value (1.5) or hour minutes string (1h 30m)
  *
  * @param {String} timeValue hour value (1.5) or hour minutes string (1h 30m)
- * @returns {string} minutes
+ * @returns {number} minutes
  */
 const getMinutes = (timeValue) => {
-    if (timeValue.split('').some((word) => ['h', 'm'].includes(word))) {
-        const hours = [...timeValue.matchAll(/\d+(h|m)/gi)]
+    if (!timeValue) return 0
+
+    const timeValueParts = [...timeValue.matchAll(/(\d+(?:\.\d+)?)+(h|m)/gi)];
+    if (timeValueParts.length) { // we are dealing with a hour minutes string
+        const hours = timeValueParts
             .map((item) =>
-                item[1] === 'h'
+                item.at(-1) === 'h'
                     ? Number(item[0].replace(/h|m/i, ''))
                     : Number(item[0].replace(/h|m/i, '')) / 60
             )
             .reduce((a, b) => a + b)
-        return hours * 60
+        return Math.floor(hours * 60)
     } else {
-        return parseFloat(timeValue) * 60
+        return Math.floor(Number(timeValue) * 60)
     }
 }
 
