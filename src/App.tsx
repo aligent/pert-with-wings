21dbb2bc70addc8794de580aa39bc560feb47b54
Thilina Gunasekara <thilina@aligent.com.au>
2023-01-13 16:15:39 +1030

    load button in the body

--- src/App.tsx
+++ src/App.tsx
@@ -1,62 +1,14 @@
-import { useState } from 'react';
-import { renderToStaticMarkup } from 'react-dom/server';
-import classnames from 'classnames';
-import ReactModal from 'react-modal';
-
-import PertTable from './PertTable';
+import PertRowsProvider from './context/pertRowsContext';
+import PertModal from './components/PertModal';
 import classes from './App.module.css';
 
-const pertModalStyles = {
-  overlay: {
-    zIndex: 9999,
-  },
-};
-
 function App() {
-  const [count, setCount] = useState(0);
-  const [isOpen, setIsOpen] = useState(false);
-
-  const handleOnClick = () => {
-    const input = document.querySelector('[aria-label="Main content area"]');
-    const html = getMarkup();
-    if (input) {
-      // clear the message box if it's just the placeholder
-      if (input.innerHTML.includes('Add a comment\u2026')) {
-        input.innerHTML = '';
-      }
-
-      input.innerHTML += html;
-      setIsOpen(false);
-    }
-  };
-
-  const getMarkup = () => {
-    return renderToStaticMarkup(<PertTable />);
-  };
-
-  const handleOpen = () => {
-    setIsOpen(true);
-  };
-
-  const handleClosePertModal = () => {
-    setIsOpen(false);
-  };
-
   return (
-    <div className={classes.App}>
-      <button className={classes.openModalButton} onClick={handleOpen}>
-        PERT
-      </button>
-      <ReactModal
-        isOpen={isOpen}
-        style={pertModalStyles}
-        appElement={document.getElementById('crx-root') || undefined}
-      >
-        <PertTable isEditing={true} />
-        <button onClick={handleOnClick}>Add comment</button>
-        <button onClick={handleClosePertModal}>Cancel</button>
-      </ReactModal>
-    </div>
+    <PertRowsProvider>
+      <div className={classes.App}>
+        <PertModal />
+      </div>
+    </PertRowsProvider>
   );
 }
 
--- src/content.tsx
+++ src/content.tsx
@@ -1,85 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import styles from './index.css?inline';
 import App from './App';
 
-let mounted = false;
+const root = document.createElement('div');
+root.id = 'crx-root';
 
-const IS_AZURE = window.location.hostname.includes('azure.com');
-const IS_JIRA = window.location.hostname.includes('atlassian.net');
+document.body.append(root);
 
-//document.querySelector('[data-test-id="issue.activity.comment"]')!.append(root)
-
-// identify an element to observe
-let elementToObserve =
-  document.querySelector('[data-test-id="issue.activity.comment"]')! ||
-  document.querySelector('[aria-label="Discussion"]')!;
-
-// create a new instance of `MutationObserver` named `observer`,
-// passing it a callback function
-const observer = new MutationObserver(() => {
-  let buttonContainer = elementToObserve;
-
-  if (IS_JIRA) {
-    const commentToolbar = elementToObserve.querySelector(
-      '[data-testid="ak-editor-main-toolbar"]'
-    );
-    if (commentToolbar) {
-      buttonContainer = commentToolbar;
-    }
-  }
-
-  if (IS_AZURE) {
-    buttonContainer = elementToObserve.parentElement!;
-  }
-  // const buttonContainer =
-  //   IS_JIRA ?  ||
-  //   (elementToObserve.nextSibling as HTMLElement)?.querySelector(
-  //     ".ms-CommandBar-primaryCommands"
-  //   );
-  // console.log(elementToObserve, mounted);
-
-  if (mounted && !buttonContainer) {
-    mounted = false;
-  }
-
-  if (!buttonContainer || mounted) return;
-
-  console.log('PERT: Adding PERT button to toolbar');
-
-  const root = document.createElement('div');
-  root.id = 'crx-root';
-  if (IS_AZURE) {
-    root.style.position = 'absolute';
-    root.style.top = '5px';
-    root.style.right = '5px';
-  }
-  buttonContainer.append(root);
-
-  ReactDOM.createRoot(document.getElementById('crx-root')!).render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  );
-
-  mounted = true;
-});
-
-// call `observe()` on that MutationObserver instance,
-// passing it the element to observe, and the options object
-
-const t = setInterval(() => {
-  console.log('PERT: Looking PERT button container');
-  elementToObserve =
-    document.querySelector('[data-test-id="issue.activity.comment"]')! ||
-    document.querySelector('[aria-label="Discussion"]')!;
-  if (!elementToObserve) return;
-
-  console.log('PERT: Found PERT button container');
-  observer.observe(elementToObserve, {
-    subtree: true,
-    childList: true,
-    attributes: true,
-  });
-  clearInterval(t);
-}, 100);
+ReactDOM.createRoot(document.getElementById('crx-root')!).render(
+  <React.StrictMode>
+    <style type="text/css">{styles}</style>
+    <App />
+  </React.StrictMode>
+);
