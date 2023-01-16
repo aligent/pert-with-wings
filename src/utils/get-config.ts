b9d10fd086c0baa4f53ee384bebd380a8635f500
Thilina Gunasekara <thilina@aligent.com.au>
2023-01-17 07:47:30 +1030

    change configs percents to base 10

--- src/utils/get-config.ts
+++ src/utils/get-config.ts
@@ -1,11 +1,10 @@
 export const getConfig = () => {
   return {
     round_to_next_minutes: 0,
-    comms_percent: 0.1,
-    code_reviews_and_fixes_percent: 0.1,
-    qa_testing_percent: 0.1,
-    qa_testing_threshold: 150,
+    comms_percent: 10,
+    code_reviews_and_fixes_percent: 10,
+    qa_testing_percent: 10,
     qa_testing_min: 15,
-    automated_tests_percent: 0.1,
+    automated_tests_percent: 10,
   };
 };
