5543993a41516a1dedae73d2824989bb213ee26e
Thilina Gunasekara <thilina@aligent.com.au>
2023-01-27 13:20:52 +1030

    switch to pert-with-wings.atlassian.net

--- src/__tests__/jira.spec.ts
+++ src/__tests__/jira.spec.ts
@@ -5,7 +5,7 @@ import { setup } from './setup';
 const JIRA_USER = process.env.JIRA_USER!;
 const JIRA_PASSWORD = process.env.JIRA_PASSWORD!;
 
-describe('test PERT with wings extension', () => {
+describe('test PERT with wings extension in JIRA', () => {
   let browser: Browser, page: Page;
   const timeout = 5000;
 
@@ -13,8 +13,7 @@ describe('test PERT with wings extension', () => {
 
   beforeAll(async () => {
     const context = await setup({
-      appUrl:
-        'https://pertwithwings.atlassian.net/browse/PWW-1' /*, slowMo: 50, devtools: true*/,
+      appUrl: 'https://pert-with-wings.atlassian.net/browse/PWW-1',
     });
 
     browser = context.browser;
@@ -45,7 +44,7 @@ describe('test PERT with wings extension', () => {
     await page.click('#login-submit');
   });
 
-  it('should render a button in the web application.', async () => {
+  it('should render a button in JIRA ticket.', async () => {
     await page.waitForFunction("window.location.pathname == '/browse/PWW-1'");
     const btn = await page.waitForSelector("[id^='pert-button-']", {
       visible: true,
@@ -55,7 +54,7 @@ describe('test PERT with wings extension', () => {
     expect(btnText).toEqual('PERT');
   });
 
-  it('should render a popup when PERT button is clicked.', async () => {
+  it('should render a popup when PERT button is clicked in JIRA ticket.', async () => {
     await page.waitForSelector("[id^='pert-button-']", {
       visible: true,
       timeout,
@@ -72,7 +71,7 @@ describe('test PERT with wings extension', () => {
     expect(addPertBtnTxt).toEqual('ADD PERT ESTIMATE');
   });
 
-  it('should post an accurate PERT estimate table.', async () => {
+  it('should post an accurate PERT estimate table in JIRA ticket.', async () => {
     // fill the PERT form
     await page.waitForSelector("[id^='task-']", {
       visible: true,
@@ -116,9 +115,6 @@ describe('test PERT with wings extension', () => {
       (e) => (e as HTMLElement).innerText
     );
 
-    // expect(pertCommentText).toEqual(
-    //   'Task\n\n\t\n\nEstimate\n\n\n\n\nAnalysis, solution design and/or scoping\n\n\t\n\n1h\n\nTask\n\n\t\n\nOptimistic Estimate\n\n\t\n\nLikely Estimate\n\n\t\n\nPessimistic Estimate\n\n\t\n\nPERT Estimate\n\n\n\n\nMy task\n\n\t\n\n1h\n\n\t\n\n2h\n\n\t\n\n6h\n\n\t\n\n2h 30m\n\n\n\n\nTicket specific communications\n\n\t\n\n0h 06m\n\n\t\n\n0h 12m\n\n\t\n\n0h 36m\n\n\t\n\n0h 15m\n\n\n\n\nCode review & fixes\n\n\t\n\n0h 06m\n\n\t\n\n0h 12m\n\n\t\n\n0h 36m\n\n\t\n\n0h 15m\n\n\n\n\nQuality Assurance Testing\n\n\t\n\n0h 15m\n\n\t\n\n0h 15m\n\n\t\n\n0h 36m\n\n\t\n\n0h 15m\n\n\n\n\nTotal Estimate, including Analysis effort\n\n\t\n\n2h 27m\n\n\t\n\n3h 39m\n\n\t\n\n8h 48m\n\n\t\n\n4h 15m\n\n\n'
-    // );
     expect(pertCommentText).toMatchSnapshot();
   });
 
