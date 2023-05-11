f63703842ba5479ea94c10e20d62d0576a6f43be
Thilina Gunasekara <thilina@aligent.com.au>
2023-05-11 16:09:44 +0930

    PERT-33: add utility to get a single string from translation object if its an array

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,6 +2,7 @@ export * from './get-config';
 export * from './get-eggs';
 export * from './get-minutes';
 export * from './get-percent';
+export * from './get-random-translation';
 export * from './get-sums';
 export * from './get-time-string';
 export * from './save-config';
