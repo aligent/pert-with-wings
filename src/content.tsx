7e503890284016ffb74302f9cab765731f354a18
Thilina Gunasekara <thilina@aligent.com.au>
2026-01-30 00:49:38 +1030

    PERT-56: move the PERT buttons to right bottom jira container when available to stop JIRA floating overlapping with PERT button

--- src/content.tsx
+++ src/content.tsx
@@ -8,7 +8,15 @@ import styles from './index.css?inline';
 const root = document.createElement('div');
 root.id = 'crx-root';
 
-document.body.append(root);
+const bottomRightContainer = document.querySelector(
+  '[data-testid="layout-controller.ui.bottom-right-corner.container.styled-container"]',
+);
+
+if (bottomRightContainer) {
+  bottomRightContainer.prepend(root);
+} else {
+  document.body.append(root);
+}
 
 createRoot(document.getElementById('crx-root') as Element).render(
   <StrictMode>
