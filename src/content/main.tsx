0c2165843bb9faed3d79e4d4602aa7132a0e9e56
Thilina Gunasekara <thilina@aligent.com.au>
2026-02-02 00:20:07 +1030

    PERT-56: move the PERT button to LHS to avoid clashing with Revo AI button

--- src/content/main.tsx
+++ src/content/main.tsx
@@ -7,15 +7,7 @@ import App from './App.tsx';
 const root = document.createElement('div');
 root.id = 'crx-root';
 
-const bottomRightContainer = document.querySelector(
-  '[data-testid="layout-controller.ui.bottom-right-corner.container.styled-container"]'
-);
-
-if (bottomRightContainer) {
-  bottomRightContainer.prepend(root);
-} else {
-  document.body.append(root);
-}
+document.body.append(root);
 
 createRoot(root).render(
   <StrictMode>
